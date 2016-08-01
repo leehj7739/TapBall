26bcbb79251eb295fa9dfe83c3150ed22735008e
leehj7739 <leehj7739@gmail.com>
2016-08-01 12:31:01 +0900

    체인지 연습12
    12

--- TapBall/ViewController.swift
+++ TapBall/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var block : UIView!
+    var animator:UIDynamicAnimator? = nil;
+    let gravity = UIGravityBehavior()
+    
+    func createAnimatorStuff() {
+        animator = UIDynamicAnimator(referenceView:self.view);
+        animator?.addBehavior(gravity)
+        
+        gravity.addItem(block);
+        gravity.gravityDirection = CGVectorMake(0, 0.8)
+        animator?.addBehavior(gravity);
+        print(gravity.items.count)
+    }
+    
+    
+    @IBAction func makeBall(sender: AnyObject) {
+        print("tap!")
+        makeRedBlock()
+        createAnimatorStuff()
+    
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func makeRedBlock(){
+        let newBlock = UIView(frame: CGRectMake(50,20,100,100))
+        newBlock.backgroundColor = UIColor.redColor()
+        self.view.addSubview(newBlock)
+        
+        block = newBlock
+    }
 
 }
 
